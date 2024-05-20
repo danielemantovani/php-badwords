6d06c00d32251939d961aef66a423b32b4efa478
danielemantovani <daniele.mantovani124@gmail.com>
2024-05-20 17:34:01 +0200

    feat: form con parola censurata

---
+++ form.php
@@ -0,0 +1,35 @@
+<?php
+//creo due variabili e gli do valore prelevando il name dall'input nell'index.php 
+
+$text = $_GET["paragraph"];
+
+$word_censured = $_GET["censured"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP form</title>
+</head>
+
+<body>
+    <section>
+        <!-- stampo in pagina il testo inserito dal'utente -->
+        <h2>Testo inserito: <p><?php echo $text; ?></p></h2>
+    </section>
+
+    <section>
+        <!-- funzione per censurare la parola digitata dall'utente -->
+        <?php
+        $censured_text = str_replace($word_censured, "***", $text);
+        ?>
+        <!-- testo trasformato con parola inserita dall'utente censurata -->
+        <h2>Testo con censura: <p><?php echo $censured_text; ?></p></h2>
+    </section>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP intro</title>
+    <title>PHP form</title>
 </head>
+
 <body>
-    <form action="">
-        <input type="text">
-        <button>Send</button>
+    <h1>Inserisci del testo e una parola che vuoi censurare all'interno di esso</h1>
+    <form action="form.php" method="GET">
+        <textarea name="paragraph" aria-label="inserimento testo" placeholder="Digitare del testo"></textarea>
+        <input type="text" name="censured" aria-label="inseirmento parola da censurare" placeholder="Parola da censurare">
+        <button type="submit">Vai</button>
     </form>
 </body>
 </html>
